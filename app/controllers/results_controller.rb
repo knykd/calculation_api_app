5ddb6596648605f2e3ab463f986c5db020c0e7e2
knykd <kok1022kok@gmail.com>
2020-03-16 22:27:14 +0900

    Add: Rspec追加

---
+++ app/controllers/results_controller.rb
@@ -0,0 +1,9 @@
+class ResultsController < ApplicationController
+  def calc; end
+
+  def grouping; end
+
+  def making_bignum; end
+
+  def most; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'calc', to: 'results#calc'
+  get 'grouping', to: 'results#grouping'
+  get 'making_bignum', to: 'results#making_bignum'
+  get 'most', to: 'results#most'
 end
